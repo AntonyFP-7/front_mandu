015f29ba3686090cf7aa1fd77b22d9a6d8424b80
RHNUBE-ANTONY <afloresrhnube@gmail.com>
2025-10-05 00:34:21 -0500

    `feat: Added NgZorro Ant Design components and styles to the application`

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCardModule,
+    NzIconModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export default class Login {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private message: NzMessageService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.message.success('¡Formulario válido! NgZorro está funcionando correctamente.');
+      console.log('Datos del formulario:', this.loginForm.value);
+    } else {
+      this.message.error('Por favor, completa todos los campos correctamente.');
+      Object.values(this.loginForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
