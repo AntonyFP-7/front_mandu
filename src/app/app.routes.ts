b75aa0132637a6873187cdcc65b15f8d352947bf
RHNUBE-ANTONY <afloresrhnube@gmail.com>
2025-10-05 00:01:47 -0500

    feat: add login and dashboard modules with routing
    - Updated package.json to include ng-zorro-antd.
    - Removed placeholder content from app.html and added a router outlet.
    - Implemented routing for login and dashboard modules in app.routes.ts.
    - Created dashboard component with basic template and styles.
    - Created login component with basic template and styles.
    - Added unit tests for login and dashboard components.
    - Defined routes for login and dashboard components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./login/routes/login.routes'),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./dashboard/routes/dashboard.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
---
+++ src/app/login/routes/login.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import Login from "../login";
+
+export default [
+    {
+        path: 'login',
+        component: Login
+    }
+] as Routes;
